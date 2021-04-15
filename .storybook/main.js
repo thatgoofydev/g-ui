83c301673d67211d4c05bdde58f038da1db97ba7
Jeroen Verwimp <jeroen@verwimp.me>
2021-04-15 23:54:39 +0200

    Added support for scss and css modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: true
+        }
+      }
+    }
   ],
   typescript: {
     check: false,
