e151551ffa2f7d8b13a7197f9b594ba36b7e677f
Jeroen Verwimp <jeroen@verwimp.me>
2021-04-15 20:59:21 +0200

    Added example of all supported types to BasicField

--- src/Field/BasicField/BasicField.stories.tsx
+++ src/Field/BasicField/BasicField.stories.tsx
@@ -4,6 +4,7 @@ import { Field } from "../Field";
 import { Form } from "../../Form/Form";
 import { FormErrors } from "../../Form";
 import { FormActions } from "../../Form/types";
+import { Button } from "../../Button";
 
 const meta: Meta = {
   title: "Form/Fields",
@@ -13,14 +14,32 @@ const meta: Meta = {
 /* BasicFieldStory */
 
 interface IFormModel {
-  name: string;
-  date?: string;
+  date: string;
+  email: string;
+  month: string;
+  number: number;
+  password: string;
+  search: string;
+  tel: string;
+  text: string;
+  time: string;
+  url: string;
+  week: string;
 }
 
 const BasicFieldStory: Story = (_) => {
   const initialValues: IFormModel = {
-    name: "Hazel Nutt",
-    date: "2021-04-15"
+    date: "2021-04-15",
+    email: "dunstan@example.com",
+    month: "2021-02",
+    number: 0,
+    password: "password",
+    search: "Search something...",
+    tel: "",
+    text: "Hazel Nutt",
+    time: "15:52",
+    url: "https://goofy.dev",
+    week: "2021-W05"
   };
 
   const onValidate = (
@@ -28,12 +47,15 @@ const BasicFieldStory: Story = (_) => {
     actions: FormActions,
     errors: FormErrors<IFormModel>
   ) => {
-    if (!values.name || values.name == "") {
-      errors.name = "Name is required";
+    if (!values.text || values.text == "") {
+      errors.text = "Name is required";
     }
   };
 
-  const onSubmit = async (values: IFormModel, actions: FormActions) => {};
+  const onSubmit = async (values: IFormModel, actions: FormActions) => {
+    alert(JSON.stringify(values, null, 2));
+    await actions.displaySuccess();
+  };
 
   return (
     <Form
@@ -41,8 +63,19 @@ const BasicFieldStory: Story = (_) => {
       onValidate={onValidate}
       onSubmit={onSubmit}
     >
-      <Field type="text" name="name" label="Text" />
       <Field type="date" name="date" label="Date" />
+      <Field type="email" name="email" label="Email" />
+      <Field type="month" name="month" label="Month" />
+      <Field type="number" name="number" label="Number" />
+      <Field type="password" name="password" label="Password" />
+      <Field type="search" name="search" label="Search" />
+      <Field type="tel" name="tel" label="Tel" />
+      <Field type="text" name="text" label="Text" />
+      <Field type="time" name="time" label="Time" />
+      <Field type="url" name="url" label="Url" />
+      <Field type="week" name="week" label="Week" />
+
+      <Button type="submit" label="Submit" />
     </Form>
   );
 };
