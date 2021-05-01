0a036e477b5296d88a8a25e6faf39685d78ca3da
Jeroen Verwimp <jeroen@verwimp.me>
2021-05-01 15:20:46 +0200

    Added RecursiveKeysOf type

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,3 +1,9 @@
 export type DeepPartialWithValueType<T, U> = {
-  [P in keyof T]?: P extends Object ? DeepPartialWithValueType<T[P], U> : U;
+  [K in keyof T]?: K extends Object ? DeepPartialWithValueType<T[K], U> : U;
 };
+
+export type RecursiveKeysOf<TObj extends object> = {
+  [TKey in keyof TObj & (string | number)]: TObj[TKey] extends object
+    ? `${TKey}` | `${TKey}.${RecursiveKeysOf<TObj[TKey]>}`
+    : `${TKey}`;
+}[keyof TObj & (string | number)];
